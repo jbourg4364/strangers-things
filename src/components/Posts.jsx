b58d0a8f86c17944b19c4c6cd8b543f87e09d8fd
Anna Gibes <gibesav@gmail.com>
2023-04-12 14:26:51 -0500

    add post form

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,8 +1,10 @@
 import React, {useEffect} from "react";
-import { getPosts } from "../api";
+import { createNewPosts, getPosts } from "../api";
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { AddPost } from './index';
 import "./Post.css";
 
-const Posts = ({posts, setPosts, isLoggedIn, user}) => {
+const Posts = ({posts, setPosts, isLoggedIn, user, token, setToken}) => {
    
     useEffect(() => {
         const getAllPosts = async () => {
@@ -11,18 +13,25 @@ const Posts = ({posts, setPosts, isLoggedIn, user}) => {
         };
         getAllPosts();
     }, []);
-
+    const navigate = useNavigate();
     return (
         <>
             <h2>All Posts</h2>
+            <button onClick={() => {
+                navigate('/addPost')
+                //only show posts they are the author of (using filter)
+            }}>Add Post</button>
+            <button>Edit Post</button>
+            <button>Delete Post</button>
+            
             {posts.map((post) => {
                 return (
                     <div className="post" key={post._id}>
                         <h2>{post.title}</h2>
                         <p>{post.description}</p>
-                        <p>{post.author.username}</p>
-                        <p>{post.location}</p>
-                        <p>{post.price}</p>
+                        <p>Posted By: {post.author.username}</p>
+                        <p>Location: {post.location}</p>
+                        <p>Price: {post.price}</p>
                         {post.willDeliver ? 
                         <>
                         <p>Delivery Available</p> 
@@ -30,6 +39,7 @@ const Posts = ({posts, setPosts, isLoggedIn, user}) => {
                         <>
                         <p>Delivery Not Available</p>
                         </>}
+
                     </div>
                     )
                 
