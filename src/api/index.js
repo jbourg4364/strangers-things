a682845e536588fb2d764524df93cadf2d7e8592
Anna Gibes <gibesav@gmail.com>
2023-04-11 20:36:50 -0500

    added the rest of the changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,23 @@
-const COHORT_NAME = '2303-FTB-ET-WEB-FT';
+// const COHORT_NAME = '2303-FTB-ET-WEB-FT';
 
-const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+// const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+
+
+// const registerUser = async () => {
+//     try { 
+//         //check if user already exists
+//         //if no exisiting user, add the new user info to state
+//     } catch (error) {
+//         console.log('error');
+//     }
+// }
+
+// const loginUser = async () => {
+//     try { 
+//         //check if user exists in state
+//         //if username and password match, route to homepage
+//     } catch (error) {
+//         console.log('error');
+//     }
+// }
 
